0aac09ec9db03123f36334a69846f845e73918e8
aviel <abrown.inbox@gmail.com>
2021-04-22 10:35:59 +0300

    Update flipflop.vhd

--- synchronious-systems/flipflop.vhd
+++ synchronious-systems/flipflop.vhd
@@ -23,9 +23,19 @@ begin
             Q <= '0';
 
         elsif clk'event and clk = '1' then
-            Q <= J and K;
 
-        end if;
+						if J = '0' and K = '1' then
+							Q <= '0';
+
+						elsif J = '1' and K = '0' then
+							Q <= '1';
+
+						elsif J = '1' and K = '1' then
+            Q <= not Q;
+
+        		end if;
+
+				end if;
 
 		end process;
 
